73b325285bff0e68ecf70354281cc576f83db39f
pankajdaundkar <phd27993@gmail.com>
2023-06-20 18:09:24 +0530

    Add project files.

---
+++ Methodoverlode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverlode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Substract_overlod sub = new Substract_overlod();
+            int result = sub.Substract(5, 2);
+            Console.WriteLine(result);
+
+            Substract_overlod sub1 = new Substract_overlod();
+            int result1 = sub1.Substract(7,8,9);
+            Console.WriteLine(result1);
+           
+            Substract_overlod mul = new Substract_overlod();
+            int res = mul.Multipy(3,6);
+            Console.WriteLine(res);
+
+            Substract_overlod mul2 = new Substract_overlod();
+            int res1 = mul2.Multiply(3,6,5);
+            Console.WriteLine(res1);
+
+            Substract_overlod add = new Substract_overlod();
+            int res3 = add.Addition(5, 1);
+            Console.WriteLine(res3);
+
+            Substract_overlod add1 = new Substract_overlod();
+            int res4 = add1.Addition(7,8,9);
+            Console.WriteLine(res4);
+        }
+         
+    }
+}
---
+++ Methodoverlode/Substract_overlod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverlode
+{
+    public class Substract_overlod
+    {
+        public int Substract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Substract(int a, int b, int c)
+        {
+            return a - b - c;
+        }
+        public int Multipy(int a, int b)
+        {
+            return a * b;
+        }
+        public int Multiply(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+        public int Addition(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public int Addition(int n1, int n2, int n3)
+        {
+            return n1 + n2;
+        }
+    }
+}
